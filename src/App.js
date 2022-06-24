f7d72a121df6db87c7dc22f72bf58c5a476838d1
Wayne <wayne.cen@gmail.com>
2022-06-24 01:17:58 -0400

    Add dice component and description of game

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import React from "react";
+import Die from "./components/Die";
+
 function App() {
-	return <main></main>;
+	return (
+		<main>
+			<p className="gameDescription">
+				Roll until all dice are the same. Click each die to freeze it at
+				its current value between rolls.
+			</p>
+			<div className="rowOne flex-row">
+				<Die value="1" />
+				<Die value="2" />
+				<Die value="3" />
+				<Die value="4" />
+				<Die value="5" />
+			</div>
+			<div className="rowTwo flex-row">
+				<Die value="5" />
+				<Die value="6" />
+				<Die value="5" />
+				<Die value="4" />
+				<Die value="3" />
+			</div>
+		</main>
+	);
 }
 
 export default App;
---
+++ src/components/Die.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Die({ value }) {
+	return <div className="dice">{value}</div>;
+}
