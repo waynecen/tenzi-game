9e77c724b800d134dbd5def55c01e68d6a872afa
Wayne <wayne.cen@gmail.com>
2022-06-25 07:22:35 -0400

    Add dots to dice faces

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 # <div align="center"><center>[Tenzi](https://tenzi-game.vercel.app/)</div>
 A game of dice where the goal is to hold ten dice of the same value. Try to get the fastest time!
 
-## 🏃 Motivation
-This project was created to practice using React, specifically handling useState, useEffect.
+## 🏃 Motivation & Challenges
+This project was created to practice using React and apply what I learned about React Hooks
+
+#### Conditionally rendering dot faces with props:
+```javascript
+// if value is 1, render class with props.value
+{value === 1 && (
+	<div className={`--${value}`}>
+		<span class="dot"></span>
+	</div>
+)}
+```
 
 ## 🚧 To Do
-- [ ] Add dots instead of numbers to dice
+- [x] Add dots instead of numbers to dice
 - [ ] Track number of rolls
 - [ ] Track time to win
 - [ ] Save best time in localStorage
--- src/components/Die.js
+++ src/components/Die.js
@@ -2,11 +2,69 @@ import React from "react";
 
 export default function Die({ value, isFrozen, holdDice }) {
 	return (
-		<div
-			className={`${isFrozen ? "isFrozen" : "dice"} `}
-			onClick={holdDice}
-		>
-			<p>{value}</p>
+		<div className={`${isFrozen ? "isFrozen" : "dice"}`} onClick={holdDice}>
+			{value === 1 && (
+				<div className={`--${value}`}>
+					<span class="dot"></span>
+				</div>
+			)}
+			{value === 2 && (
+				<div className={`--${value}`}>
+					<span class="dot"></span>
+					<span class="dot"></span>
+				</div>
+			)}
+			{value === 3 && (
+				<div className={`--${value}`}>
+					<span class="dot"></span>
+					<span class="dot"></span>
+					<span class="dot"></span>
+				</div>
+			)}
+			{value === 4 && (
+				<div className={`--${value}`}>
+					<div className="column">
+						<span class="dot"></span>
+						<span class="dot"></span>
+					</div>
+					<div className="column">
+						<span class="dot"></span>
+						<span class="dot"></span>
+					</div>
+				</div>
+			)}
+			{value === 5 && (
+				<div className={`--${value}`}>
+					<div class="column">
+						<span class="dot"></span>
+						<span class="dot"></span>
+					</div>
+
+					<div class="column">
+						<span class="dot"></span>
+					</div>
+
+					<div class="column">
+						<span class="dot"></span>
+						<span class="dot"></span>
+					</div>
+				</div>
+			)}
+			{value === 6 && (
+				<div className={`--${value}`}>
+					<div className="column">
+						<span class="dot"></span>
+						<span class="dot"></span>
+						<span class="dot"></span>
+					</div>
+					<div className="column">
+						<span class="dot"></span>
+						<span class="dot"></span>
+						<span class="dot"></span>
+					</div>
+				</div>
+			)}
+			{/* <p>{value}</p> */}
 		</div>
 	);
 }
